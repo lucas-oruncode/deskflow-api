3360a534c9c1b8e95bfb395fdb014f77635d6d9a
Lucas de Jesus <oruncode@gmail.com>
2026-09-21 20:55:39 -0300

    feat: configura entity framework e appdbcontext

--- src/Deskflow.API/Program.cs
+++ src/Deskflow.API/Program.cs
@@ -1,4 +1,9 @@
+using Deskflow.API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
