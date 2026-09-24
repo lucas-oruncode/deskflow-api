e64195de278d90067b36c500b712d5fd3a265927
Lucas de Jesus <oruncode@gmail.com>
2026-09-24 19:10:23 -0300

    feat: configura swagger

--- src/Deskflow.API/Program.cs
+++ src/Deskflow.API/Program.cs
@@ -6,6 +6,8 @@ using Deskflow.API.Services.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
@@ -14,7 +16,13 @@ builder.Services.AddScoped<ICategoryService, CategoryService>();
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
+    app.MapOpenApi();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "Deskflow API V1");
+    });
 }
 
+app.MapControllers();
 
 app.Run();
