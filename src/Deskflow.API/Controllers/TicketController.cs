3a34d1f2365196f500ab9ab825f312adb77e8ef2
Lucas de Jesus <oruncode@gmail.com>
2026-09-26 16:38:03 -0300

    feat: implementa criação de ticket

---
+++ src/Deskflow.API/Controllers/TicketController.cs
@@ -0,0 +1,52 @@
+using Deskflow.API.DTOs.Tickets;
+using Deskflow.API.Models;
+using Deskflow.API.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Deskflow.API.Controllers
+
+{
+    [ApiController]
+    [Route("api/ticket")]
+    public class TicketController : ControllerBase
+    {
+
+        private readonly ITicketService _ticketService;
+
+        public TicketController(ITicketService ticketService)
+        {
+            _ticketService = ticketService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateTicket([FromBody] TicketDto ticketDto)
+        {
+            var ticket = new Ticket
+            {
+                Title = ticketDto.Title,
+                Description = ticketDto.Description,
+                Requester = ticketDto.Requester,
+                Priority = ticketDto.Priority,
+                CategoryId = ticketDto.CategoryId
+            };
+
+            await _ticketService.CreateAsync(ticket);
+            return Created($"/api/ticket/{ticket.Id}", 
+                    new TicketResponseDto
+                    {
+                       Id = ticket.Id,
+                       Title = ticket.Title,
+                       Description = ticket.Description,
+                       Requester = ticket.Requester,
+                       Status = ticket.Status,
+                       Priority = ticket.Priority,
+                       CategoryId = ticket.CategoryId,
+                       CreatedAt = ticket.CreatedAt,
+                       ClosedAt = ticket.ClosedAt
+                    });
+        }
+
+        
+        
+    }
+}
